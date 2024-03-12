85780b92e204523136bb11d63c15f56a3f68d2d0
ThEo_BrNg <161337961+ThEbRnGGMS@users.noreply.github.com>
2024-03-12 10:29:33 +0100

    Update Decompte.py
    Rajout de l'heure actuelle !!!

--- Decompte.py
+++ Decompte.py
@@ -1,13 +1,22 @@
 import time
 import os
+from datetime import datetime
 
-tmp_sec = int(input("Entre le temps en sec : "))
+mode = int(input("Veux tu l'heure (1) ou un decompte (2) ? "))
 
-while tmp_sec != 0:
-    tmp_sec -= 1
-    time.sleep(1)
+if mode == 1:
+    heure_actuelle = datetime.now().strftime("%H:%M:%S")
+    print("L'heure actuelle est :", heure_actuelle)
+
+if mode == 2:
+    
+    tmp_sec = int(input("Entre le temps en sec : "))
+
+    while tmp_sec != 0:
+        tmp_sec -= 1
+        time.sleep(1)
+        os.system('cls')
+        print(tmp_sec)
+
     os.system('cls')
-    print(tmp_sec)
-
-os.system('cls')
-print("temps écouler !!!")
+    print("temps écouler !!!")
